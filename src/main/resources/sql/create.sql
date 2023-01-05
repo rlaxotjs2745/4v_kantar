76d11da32f2fac8dce37d08041ce1d46b6a01d2f
WiNMasTeR-SW <sungwoong@4thevision.com>
2023-01-05 11:25:53 +0900

    sql

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -16,7 +16,8 @@ ENGINE=InnoDB
 
 CREATE TABLE `KT_PROJECT` (
 	`idx_project` INT(11) NOT NULL AUTO_INCREMENT,
-	`idx_project_job_projectid` INT(11) NULL DEFAULT NULL,
+	`idx_project_job_projectid` INT(11) NOT NULL,
+	`project_name` VARCHAR(100) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
 	`project_type` TINYINT(4) NULL DEFAULT '0' COMMENT '0:임시, 1: 단일, 2:병합',
 	`project_status` TINYINT(4) NULL DEFAULT '0' COMMENT '0:임시 저장상태, 1: 파일 파싱이 정상적으로 되어서 정식 프로젝트로 저장됨, 2:병합이 되어버림, 10:병합 임시, 11: 병합완료되어정상',
 	`idx_user` INT(11) NOT NULL DEFAULT '0',
@@ -24,13 +25,15 @@ CREATE TABLE `KT_PROJECT` (
 	`summary2` VARCHAR(400) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
 	`summary3` VARCHAR(400) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
 	`summary4` VARCHAR(400) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
-	`create_dt` DATETIME NOT NULL DEFAULT current_timestamp(),
-	`update_dt` DATETIME NOT NULL,
-	PRIMARY KEY (`idx_project`) USING BTREE
+	`create_dt` DATETIME NULL DEFAULT current_timestamp(),
+	`update_dt` DATETIME NULL DEFAULT NULL ON UPDATE current_timestamp(),
+	PRIMARY KEY (`idx_project`) USING BTREE,
+	INDEX `idx_project_job_projectid` (`idx_project_job_projectid`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
 ;
+
 
 CREATE TABLE `KT_PROJECT_JOB` (
 	`idx_project_job` INT(11) NOT NULL AUTO_INCREMENT,
@@ -45,6 +48,7 @@ CREATE TABLE `KT_PROJECT_JOB_PROJECTID` (
 	`idx_project_job_projectid` INT(11) NOT NULL AUTO_INCREMENT,
 	`idx_project_job` INT(11) NOT NULL,
 	`project_id` VARCHAR(10) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`idx_user` INT(11) NOT NULL DEFAULT '0',
 	PRIMARY KEY (`idx_project_job_projectid`) USING BTREE,
 	INDEX `FK_KT_PROJECT_JOB_PROJECTID_KT_PROJECT_JOB` (`idx_project_job`) USING BTREE,
 	CONSTRAINT `FK_KT_PROJECT_JOB_PROJECTID_KT_PROJECT_JOB` FOREIGN KEY (`idx_project_job`) REFERENCES `KT_PROJECT_JOB` (`idx_project_job`) ON UPDATE NO ACTION ON DELETE CASCADE
@@ -55,8 +59,11 @@ ENGINE=InnoDB
 
 CREATE TABLE `KT_REPORT` (
 	`idx_report` INT(11) NOT NULL AUTO_INCREMENT,
-	`file_path` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`file_name` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`idx_project` INT(11) NOT NULL,
+	`idx_project_job_projectid` INT(11) NOT NULL,
+	`filepath` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`filename` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`idx_user` INT(11) NOT NULL DEFAULT '0',
 	`create_dt` DATETIME NULL DEFAULT current_timestamp(),
 	`update_dt` DATETIME NULL DEFAULT NULL ON UPDATE current_timestamp(),
 	PRIMARY KEY (`idx_report`) USING BTREE
@@ -64,3 +71,4 @@ CREATE TABLE `KT_REPORT` (
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
 ;
+
