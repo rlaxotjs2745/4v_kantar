3912ed65edcda437803c8771d18e687d3834a8b2
teaseon <taeseon@4thevision.com>
2023-01-12 21:22:59 +0900

    dictionary, 멤버관리 완료

---
+++ src/main/java/com/kantar/vo/DictionaryDataVO.java
@@ -0,0 +1,22 @@
+package com.kantar.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class DictionaryDataVO extends BaseVO {
+    private Integer idx_dictionary_data;
+    private Integer idx_dictionary;
+    private String keyword;
+    private String keyword01;
+    private String keyword02;
+    private String keyword03;
+    private String keyword04;
+    private String keyword05;
+    private String keyword06;
+    private String keyword07;
+    private String keyword08;
+    private String keyword09;
+    private String keyword10;
+}
